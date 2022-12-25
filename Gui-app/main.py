72eba976d6f3bcfde83738cf911dd82bfcee0479
Simon Atiegar <atiegardelali@gmail.com>
2022-12-25 01:06:29 +0000

    add save and load options

--- Gui-app/main.py
+++ Gui-app/main.py
@@ -11,13 +11,26 @@ class NoteApp(QMainWindow):
         self.text_edit = QTextEdit(self)
         self.setCentralWidget(self.text_edit)
 
+        # create a Qaction for saving and loading
+        #  the application
+        save_action = QAction("Save", self)
+        save_action.triggered.connect(self.close)
+        load_action = QAction("Load",self)
+        load_action.triggered.connect(self.close)
+
         # create a QAction for quitting the application 
         # and add 
-        # it to the File menu
+        # it to the file menu
         exit_action = QAction("Exit", self)
         exit_action.triggered.connect(self.close)
-        self.file_menu = self.menuBar().addMenu("Options")
+        self.file_menu = self.menuBar().addMenu("File")
+        self.formatting_menu = self.menuBar().addMenu("Formatting")
+
+        # add it to the file menu
+        self.file_menu.addAction(save_action)
+        self.file_menu.addAction(load_action)
         self.file_menu.addAction(exit_action)
+        
 
 if True:
     app = QApplication(sys.argv)
