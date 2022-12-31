6af9afe75ff37132124ee4cd5156be7c4206d14e
Simon Atiegar <atiegardelali@gmail.com>
2022-12-31 00:49:10 +0000

    add load and save buttons

--- Gui-app/main.py
+++ Gui-app/main.py
@@ -1,5 +1,5 @@
 import sys 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit,QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog
 
 print('Starting...')
 class NoteApp(QMainWindow):
@@ -14,9 +14,9 @@ class NoteApp(QMainWindow):
         # create a Qaction for saving and loading
         #  the application
         save_action = QAction("Save", self)
-        save_action.triggered.connect(self.close)
+        save_action.triggered.connect(self.save_notes)
         load_action = QAction("Load",self)
-        load_action.triggered.connect(self.close)
+        load_action.triggered.connect(self.load_notes)
 
         # create a QAction for quitting the application 
         # and add 
@@ -24,14 +24,33 @@ class NoteApp(QMainWindow):
         exit_action = QAction("Exit", self)
         exit_action.triggered.connect(self.close)
         self.file_menu = self.menuBar().addMenu("File")
-        #self.formatting_menu = self.menuBar().addMenu("Formatting")
+        self.formatting_menu = self.menuBar().addMenu("Formatting")
 
         # add it to the file menu
         self.file_menu.addAction(save_action)
         self.file_menu.addAction(load_action)
         self.file_menu.addAction(exit_action)
-        
-  
+
+        # add it to the formatting menu
+        self.formatting_menu.addAction(save_action)
+    
+    def save_notes(self):
+        options = QFileDialog.options()
+        options |= QFileDialog.ReadOnly
+        file_name, _ = QFileDialog.getSaveFileName(self,"Save Notes","Text Files(*.txt);;All Files(*)", options=options)
+        if file_name:
+            with open(file_name, 'w') as f:
+                f.write(self.text_edit.toPlainText())
+    
+    def load_notes(self):
+        options = QFileDialog.options()
+        options |= QFileDialog.ReadOnly
+        file_name, _ = QFileDialog.getOpenFileName(self,"Load Notes","Text Files(*.txt);;All Files(*)", options=options)
+        if file_name:
+            with open(file_name, 'r') as f:
+                self.text_edit.setPlainText(f.read())
+
+
 if True:
     app = QApplication(sys.argv)
     window = NoteApp()
